d85dcf6a71b4c784a98ea5e9ef6a05fc094443e9
irena <irena.vent@gmail.com>
2021-09-19 22:07:08 +0200

    DictReader IV

--- balance/models.py
+++ balance/models.py
@@ -2,8 +2,9 @@ import csv
 
 from . import FICHERO
 
-# class Movimineto no se está usando
+
 class Movimiento():
+    # class Movimineto no se está usando
     def __init__(self, fecha, concepto, es_ingreso, cantidad):
         # debemos validar todos estos valores
         self.fecha = fecha
@@ -11,28 +12,28 @@ class Movimiento():
         self.es_ingreso = es_ingreso
         self.cantidad = cantidad
 
+
 class ListaMovimientos():
     def __init__(self):
         self.movimientos = []
 
-    def leer (self):
-        self.movimientos = []
+    def leer(self):
+        self.movimientos = []  # debemos vaciar para no duplicar en cada llamada
         fichero = open(FICHERO, "r")
         dreader = csv.DictReader(fichero)
         for linea in dreader:
-            # deberes hacer que genere un instancia de mMovimiento
             self.movimientos.append(linea)
+            # deberes hacer que genere un instancia de mMovimiento
         fichero.close()
 
-    def escribir (self):
+    def escribir(self):
         if len(self.movimientos) == 0:
-            return # no sigue 
-        
+            return  # no sigue
+
         fichero = open(FICHERO, "w")
-        nombres_campos = list(self.movimiento[0].keys()) #convertir en lista
-        dwriter = csv.DictWriter(fichero, fieldnames = nombres_campos)
+        nombres_campos = list(self.movimiento[0].keys())  # convertir en lista
+        dwriter = csv.DictWriter(fichero, fieldnames=nombres_campos)
 
         for movimiento in self.movimientos:
             dwriter.writerow(movimiento)
         fichero.close()
-
---
+++ ejercicio_dic_mov.py
@@ -0,0 +1,30 @@
+import csv
+
+fichero = open("data/movimientos.csv", "r")
+reader = csv.reader(fichero)
+
+lista_de_listas = []
+dic_model = {}
+lista_dict = []
+
+for lista in reader:
+    lista_de_listas.append(lista)
+
+for item in lista_de_listas[0]:
+    dic_model[item] = ""
+lista_de_listas.remove(lista_de_listas[0])
+
+var = 0
+v = 0
+while var < len(dic_model):
+    concepto = {}
+    i = 0
+    for key in dic_model:
+        lista = [valor[i] for valor in lista_de_listas]  # valor x columnas
+        concepto[key] = f"{lista[v * 1]}"
+        i += 1
+    v += 1
+    var += 1
+    lista_dict.append(concepto)
+
+print(lista_dict)
